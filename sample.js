8d6672f6902ac28057104f139939c1d92b0216fc
mayurtummewar <mayurtummewar@gmail.com>
2025-06-02 02:02:22 +0530

    PvP mode implemented

---
+++ sample.js
@@ -0,0 +1,4 @@
+const pads = document.querySelectorAll(".pad");
+const status_box = document.querySelector("#status");
+const reset_button = document.querySelector("#reset");
+console.log(pads)
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const pads = document.querySelectorAll(".pad")
 const status = document.querySelector("#status")
 const reset = document.querySelector("#reset")
-let padClicked, winner
-let X = new Set(), O = new Set()
+let padClicked, winner = null
+let X = new Set(), O = new Set(), winArray = []
 const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -25,31 +25,46 @@ function checkWin() {
     const playerSet = (turn === "X") ? X : O;
     for (const condition of winConditions) {
         if (condition.every(index => playerSet.has(index))) {
+            winArray = condition
             return turn;
         }
     }
     return null;
 }
 
-function resetFunction() {
-    window.location.reload();
-}
-
-reset.addEventListener('click', function () {
-    resetFunction();
+reset.addEventListener('click', function resetFunction() {
+    pads.forEach(pad => {
+        pad.innerText = '';
+        pad.style.backgroundColor = "aliceblue";
+    });
+    X.clear();
+    O.clear();
+    winner = null;
+    turn = "X";
+    status.innerText = "X's turn";
 })
 
 pads.forEach(pad => {
     pad.addEventListener('click', function () {
-        padClicked = Array.prototype.indexOf.call(this.parentNode.children, this);
-        (turn === "X") ? X.add(padClicked) : O.add(padClicked);
-        pads[padClicked].innerText = turn;
-        winner = checkWin();
-        if(!winner) {
-            changeTurn();
-        }
-        else {
-            status.innerText = winner + " Won!!!";
+        if (!winner) {
+                padClicked = Array.prototype.indexOf.call(this.parentNode.children, this);
+                if (!(X.has(padClicked) || O.has(padClicked))) {
+                    (turn === "X") ? X.add(padClicked) : O.add(padClicked);
+                    pads[padClicked].innerText = turn;
+                    winner = checkWin();
+                    if(!winner) {
+                        if (X.size + O.size === 9) {
+                            status.innerText = "Tie";
+                        } else changeTurn();
+                    }
+                    else {
+                        status.innerText = winner + " Won!!!";
+                        winArray.forEach(winPad => {
+                            pads[winPad].style.backgroundColor = "cyan";
+                        })
+                    }
+                }
+            
         }
     })
 })
